9d442b6f178c059d49d5679522578058e402c35a
Gurvinder Singh Yadav <105541779+gurvinder-singh-yadav@users.noreply.github.com>
2023-05-03 16:01:09 +0530

    mvp model, demo

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Sentiment Stance Detection
+The following project contains code and demo of CS455 course where we have prepared a sentiment and stance prediction models respectively to effectively analyse the standing and emotion of the person on Climate Change Data
+
+## Run
+```
+streamlit run app.py
+```
+
+## Model Architecture
+
+![File Not Found](assets/sentiment_model_arch.png "Model Arch.")
+
+## Results
+
+### Sentiment
+![Loss file Not found](assets/sentiment_accuracy.png "Sentiment Training Accuracy")
+
+![Loss file Not found](assets/sentiment_loss.png "Sentiment Training Loss")
+
+### Stance
+![Loss file Not found](assets/stance_accuracy.png "Stance Training Accuracy")
+
+![Loss file Not found](assets/stance_loss.png "Stance Training Loss")
+
---
+++ app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+stance_model = tf.keras.models.load_model('data/Bi_LSTM_stance.h5')
+sentiment_model = tf.keras.models.load_model('data/Bi_LSTM_senti.h5')
+
+def preprocess_text(text):
+
+    max_length = 120
+    trunc_type='post'
+    padding_type='post'
+
+    # loading
+    with open('data/tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+    text_sequences = tokenizer.texts_to_sequences([text])
+    text_padded = pad_sequences(text_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+    return text_padded
+
+def predict_stance(tweet):
+    # Preprocess the tweet text
+    preprocessed_tweet = preprocess_text(tweet)
+    
+    # Make a prediction with the loaded model
+    prediction = stance_model.predict(preprocessed_tweet)[0][0]
+    
+    # Determine the Stance label based on the prediction
+    if prediction >= 0.5:
+        return ('Believer', prediction)
+    else:
+        return ('Denier', 1-prediction)
+    
+def predict_senti(tweet):
+    # Preprocess the tweet text
+    preprocessed_tweet = preprocess_text(tweet)
+
+    # Make a prediction with the loaded model
+    prediction = sentiment_model.predict(preprocessed_tweet)[0]
+    
+    # Determine the sentiment label based on the prediction
+    label = np.argmax(prediction)
+    if label == 0:
+        return ('Neutral',prediction)
+    elif label == 1:
+        return ('Positive',prediction)
+    else:
+        return ('Negative',prediction)
+
+
+def run(tweet):
+    sentiment, senti_score = predict_senti(tweet)
+    stance, stance_score = predict_stance(tweet)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric("Sentiment", sentiment, np.max(senti_score).tolist())
+    with col2:
+        st.metric("Stance", stance, float(stance_score))
+
+st.set_page_config(
+    page_title="Sentiment & Stance Detection",
+    layout="centered"
+)
+
+st.title("Sentiment & Stance Detection")
+
+col1, col2 = st.columns(2)
+
+tweet = st.selectbox(
+    "Predict Sentiment & Stance",
+    ["Climate Change is a hoax",
+     "There is not global warming",
+     "Scientists suggest that earth is warming up every consecutive year"
+     ]
+)
+
+
+if st.button("Predict"):
+    with st.spinner("Analysing Text"):
+        run(tweet)
+        st.success("Analysed")
+
+
+
+
+
